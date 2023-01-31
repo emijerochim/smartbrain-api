15757c72e53f7bc828c211db140e4db095d91c93
emijerochim <emijerochim@gmail.com>
2023-01-30 21:21:57 -0300

    cors options

--- server.js
+++ server.js
@@ -28,11 +28,7 @@ db.connect((err) => {
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://smartbrain-production.up.railway.app",
-  })
-);
+app.use(cors());
 
 app.options("*", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
