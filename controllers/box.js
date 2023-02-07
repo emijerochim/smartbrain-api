735d2f93a7dbbddaa467817c0e8a085d08a17198
emijerochim <emijerochim@gmail.com>
2023-02-07 03:01:35 -0300

    search by file

--- controllers/box.js
+++ controllers/box.js
@@ -23,6 +23,7 @@ const box = (req, res) => {
       })
       .catch((err) => res.status(400).json("unable to work with API"));
   } else if (req.body.inputs[0].data.image) {
+    console.log(req.body.inputs[0].data.image.base64);
     app.models
       .predict(
         {
@@ -31,7 +32,7 @@ const box = (req, res) => {
           version: "6dc7e46bc9124c5c8824be4822abe105",
           type: "visual-detector",
         },
-        req.body.inputs[0].data.image.url
+        req.body.inputs[0].data.image.base64
       )
       .then((data) => {
         res.json(data);
