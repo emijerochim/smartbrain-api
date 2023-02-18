9bba95684c8133d3741ceb4f750814eb9a2dc526
emijerochim <emijerochim@gmail.com>
2023-02-18 16:12:40 -0300

    errors

--- controllers/register.js
+++ controllers/register.js
@@ -6,34 +6,41 @@ const register = async (req, res, db) => {
   const { username, email, password } = req.body;
   const id = uuidv4();
 
-  const isRegistrationValid = async (email, password) => {
+  const isEmailValid = async (email) => {
     const emailRegex =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-    return emailRegex.test(email) && passwordRegex.test(password);
+    return emailRegex.test(email);
   };
 
-  if (!(await isRegistrationValid(email, password))) {
-    console.log("\nUser not added 🚫");
-    return res.status(400).json("Registration request is invalid");
-  } else {
-    const hash = bcrypt.hashSync(password, 10);
+  const isPasswordValid = async (password) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return passwordRegex.test(password);
+  };
 
-    const users = await db.query(
-      "INSERT INTO users (id, email, password, username) VALUES($1, $2, $3, $4) RETURNING *",
-      [id, email, hash, username]
-    );
-    const user = users.rows[0];
-
-    const token = jwt.sign({ id: user.id }, "secretKey", {
-      expiresIn: 86400, // expires in 24 hours
-    });
-
-    res.status(200).json({ user: user, token });
-
-    console.log("\nUser added ✅");
+  if (!(await isEmailValid(email))) {
+    console.log("\nIncorrect format for email 🚫");
+    return res.status(401).json("Incorrect format for email");
   }
+  if (!(await isPasswordValid(password))) {
+    console.log("\nIncorrect format for password 🚫");
+    return res.status(402).json("Incorrect format for password");
+  }
+  const hash = bcrypt.hashSync(password, 10);
+
+  const users = await db.query(
+    "INSERT INTO users (id, email, password, username) VALUES($1, $2, $3, $4) RETURNING *",
+    [id, email, hash, username]
+  );
+  const user = users.rows[0];
+
+  const token = jwt.sign({ id: user.id }, "secretKey", {
+    expiresIn: 86400, // expires in 24 hours
+  });
+
+  res.status(200).json({ user: user, token });
+
+  console.log("\nUser added ✅");
 };
 
 export default register;
