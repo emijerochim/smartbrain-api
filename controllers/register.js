83045687e10a6724917986bc05fc2855cf01a5a2
emijerochim <emijerochim@gmail.com>
2023-02-18 16:42:05 -0300

    errors

--- controllers/register.js
+++ controllers/register.js
@@ -14,6 +14,14 @@ const register = async (req, res, db) => {
     return user ? false : true;
   };
 
+  const isEmailAvailable = async (email) => {
+    const users = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    const user = users.rows[0];
+    return user ? false : true;
+  };
+
   const isEmailValid = async (email) =>
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
       email
@@ -22,11 +30,18 @@ const register = async (req, res, db) => {
   const isPasswordValid = async (password) =>
     /(?=.*\d)(?=.*[a-zA-Z]).{8,}/.test(password);
 
+  const isUsernameValid = async (username) => {
+    username.length > 3 && username.length < 20;
+  };
+
   if (!(await isUsernameAvailable(username))) {
     console.log("\nUsername not available on registration 🚫");
     return res.status(402).json("Username not available on registration 🚫");
   }
-
+  if (!(await isEmailAvailable(email))) {
+    console.log("\nEmail not available on registration 🚫");
+    return res.status(403).json("Email not available on registration 🚫");
+  }
   if (!(await isEmailValid(email))) {
     console.log("\nIncorrect format for email on registration 🚫");
     return res
@@ -38,6 +53,12 @@ const register = async (req, res, db) => {
     return res
       .status(401)
       .json("Incorrect format for password on registration 🚫");
+  }
+  if (!(await isUsernameValid(username))) {
+    console.log("\nIncorrect format for username on registration 🚫");
+    return res
+      .status(404)
+      .json("Incorrect format for username on registration 🚫");
   }
 
   const users = await db.query(
